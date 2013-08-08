9c23befe7c571b80a4539f2f43bf158b0d0575c6
amna <amna.mirza@tkxel.com>
2013-08-08 14:13:33 +0500

    My First Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AmnaFirstApp::Application.config.secret_key_base = '3a0c225308bb6147c477bc860a4321eefecb44b3aeb3a23aae0bc952412ced02f57d4f218bca7d2428865eeb4a10421c644f9d55ca2f2806d7dbd1c1242c837a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AmnaFirstApp::Application.config.session_store :cookie_store, key: '_amna_first_app_session'
